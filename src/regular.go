464fe4529f7fbb05d4a5a8874999c7cf9c4a0b47
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2020-06-05 01:59:58 +0900

    fix: time bug and add some document :neckbeard:

--- src/regular.go
+++ src/regular.go
@@ -25,11 +25,15 @@ func NewRegular(p *Policy) *Regular {
 }
 
 func (r *Regular) Next() time.Duration {
-	r.nextInterval = r.getRandomizedInterval(r.nextInterval)
-	return r.nextInterval
+	d := r.getRandomizedInterval(r.nextInterval)
+	r.nextInterval = time.Duration(math.Min(float64(r.nextInterval)+float64(r.regularInterval), float64(r.maxInterval)))
+	return d
 }
 
 func (r *Regular) getRandomizedInterval(i time.Duration) time.Duration {
-	f := rand.New(rand.NewSource(time.Now().UnixNano())).Float64()
-	return time.Duration(math.Min(float64(i)+float64(r.regularInterval)+float64(r.maxJitterInterval)*f, float64(r.maxInterval)))
+	rd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	if rd.Float64() > 0.5 {
+		return time.Duration(float64(i) + float64(r.regularInterval) - float64(r.maxJitterInterval)*rd.Float64())
+	}
+	return time.Duration(float64(i) + float64(r.regularInterval) + float64(r.maxJitterInterval)*rd.Float64())
 }
